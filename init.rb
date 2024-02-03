95d2ed1cbf9420c08856e5320ecc353bfab8de75
Vignesh <vemcse@gmail.com>
2024-02-03 07:37:30 +0530

    initial plugin conf

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_timelog_restrict_date do
+  name 'Redmine Timelog Restrict Date plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
