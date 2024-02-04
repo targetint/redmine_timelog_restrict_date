aef773cd305799c15c79c72da99d6a713decb73c
Rohit Thakral <rohit.thakral@targetintegration.com>
2024-02-04 08:15:30 +0000

    Update init.rb

--- init.rb
+++ init.rb
@@ -4,7 +4,8 @@ require 'restrict_time_entry_patch'
 Redmine::Plugin.register :redmine_timelog_restrict_date do
   name 'Redmine Timelog Restrict Date plugin'
   author 'Target Integration'
-  description 'Redmine Timelog Restrict Date plugin'
+  author_url 'https://www.targetintegration.com'
+  description 'Redmine Timelog Restrict Date plugin was created by the team at Target Integration www.targetintegration.com.'
   version '0.0.1'
   settings default: {}, partial: 'settings/timelog_restrict_settings.html.erb'
 end
